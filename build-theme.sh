fdae3d01c20ef3a65d64375e681cc7dc9ab08e00
Hoang Kiem <kiemhd@outlook.com>
2020-04-14 17:01:38 +0700

    Add build theme and new WordPress site with Laravel Valet

---
+++ build-theme.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Build a underscore _s theme 
+# Work with WP-CLI so you need to install WP-CLI first
+
+if [ "$#" -ne 2 ]
+then
+  echo "Usage: build-theme {theme-name} {version}"
+  exit 1
+fi
+NAME=$1
+VERSION=$2
+PACKAGE="$NAME-$VERSION"
+rm -f *.zip
+rm -rf build-room
+mkdir build-room
+wp i18n make-pot --domain=$NAME ./$NAME $NAME/languages/$NAME.pot
+cp -r $NAME build-room/$NAME
+cd build-room/$NAME
+yarn build:production
+find . -name '.DS_Store' -type f -delete.
+rm -rf node_modules .git .idea assets/scss *.json *.lock *.map *.scss .DS_Store .gitignore .stylelintrc.json *.txt
+cd ..
+zip -r $PACKAGE.zip $NAME
+mv $PACKAGE.zip ..
+cd ..
+rm -rf build-room
+echo "Build successfully!"
+~
---
+++ new-wp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# You need to install WP-CLI before run this script
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: new-wp {project-name}"
+  exit 1
+fi
+
+CHARSET='urf8'
+DB_ROOT_PASS=''
+DB_USER='root'
+DB_HOST='localhost'
+DB_PREFIX='wp_'
+USER_MAIL='kiemhd@outlook.com'
+USER_PASS='123456'
+USER_NAME='admin'
+PROJECT_NAME=$1
+
+mysql -uroot -e "CREATE DATABASE ${PROJECT_NAME} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+mkdir $PROJECT_NAME
+cd $PROJECT_NAME
+wp core download
+wp core config --dbhost=$DB_HOST --dbname=$PROJECT_NAME --dbuser=$DB_USER --dbpass=$DB_ROOT_PASS
+chmod 644 wp-config.php
+wp core install --url="${PROJECT_NAME}.test" --title="Project ${PROJECT_NAME}" --admin_name=$USER_NAME --admin_password=$USER_PASS --admin_email=$USER_MAIL
+open http://${PROJECT_NAME}.test
+history -c && exit
