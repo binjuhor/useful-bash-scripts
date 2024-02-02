a1e5e3edd5e3e06ffd0a805c0bb3db91ff2d7172
Hoang Kiem <kiemhd@outlook.com>
2024-02-02 15:08:50 +0700

    Update v.sh

--- v.sh
+++ v.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-# Short command of Laravel Valet
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 start/stop/restart"
+    echo "Usage: $0 start/stop/restart/down/up/isolate/use"
     exit 1
 fi
 
-if [ $1 != "start" ] && [ $1 != "stop" ] && [ $1 != "restart" ] && [ $1 != "use" ] && [ $1 != "up" ] && [ $1 != "down" ]; then
-    echo "Usage: $0 start/stop/restart"
+if [ $1 != "start" ] && [ $1 != "stop" ] && [ $1 != "restart" ] && [ $1 != "use" ] && [ $1 != "down" ] && [ $1 != "up" ] && [ $1 != "isolate" ]  && [ $1 != "use" ]; then
+    echo "Usage: $0 start/stop/restart/down/up/isolate/use"
     exit 1
 fi
 
 if [ $1 = "start" ] || [ $1 = "up" ]; then
-    valet start
+    valet start && brew services start mailhog
 fi
 
 if [ $1 = "stop" ] || [ $1 = "down" ]; then
-    valet stop && valet stop dnsmasq
+    valet stop && valet stop dnsmasq && brew services stop mailhog
 fi
 
 if [ $1 = "restart" ]; then
-    valet restart
+    valet restart && brew services restart mailhog
 fi
 
 if [ $1 = "use" ]; then
     valet use $2 && composer global update
+fi
+
+if [ $1 = "isolate" ]; then
+    valet isolate $2
+fi
+
+if [ $1 = "unisolate" ]; then
+    valet unisolate
+fi
+
+if [ $1 = "share" ]; then
+    valet share-tool ngrok
 fi
