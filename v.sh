a290d0bef683df009e2845b8a5586477c0ab6784
Hoang Kiem <kiemhd@outlook.com>
2024-06-08 17:34:39 +0700

    Update v.sh
    Update v.sh

--- v.sh
+++ v.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 start/stop/restart/down/up/isolate/use/share"
+    echo "Usage: $0 start/stop/restart/down/up/isolate/use/share/secure"
     exit 1
 fi
 
-if [ $1 != "start" ] && [ $1 != "stop" ] && [ $1 != "restart" ] && [ $1 != "use" ] && [ $1 != "down" ] && [ $1 != "up" ] && [ $1 != "isolate" ]  && [ $1 != "use" ]  && [ $1 != "share" ]; then
-    echo "Usage: $0 start/stop/restart/down/up/isolate/use"
+if [ $1 != "start" ] && [ $1 != "stop" ] && [ $1 != "restart" ] && [ $1 != "use" ] && [ $1 != "down" ] && [ $1 != "up" ] && [ $1 != "isolate" ]  && [ $1 != "use" ]  && [ $1 != "share" ]  && [ $1 != "secure" ]; then
+    echo "Usage: $0 start/stop/restart/down/up/isolate/use/share/secure"
     exit 1
 fi
 
 if [ $1 = "start" ] || [ $1 = "up" ]; then
-    valet start && brew services start mailhog
+    valet start
 fi
 
 if [ $1 = "stop" ] || [ $1 = "down" ]; then
-    valet stop && valet stop dnsmasq && brew services stop mailhog
+    valet stop && valet stop dnsmasq
 fi
 
 if [ $1 = "restart" ]; then
-    valet restart && brew services restart mailhog
+    valet restart
 fi
 
 if [ $1 = "use" ]; then
@@ -35,4 +35,8 @@ fi
 
 if [ $1 = "share" ]; then
     valet share
+fi
+
+if [ $1 = "secure" ]; then
+    valet secure
 fi
